b18bff79a78c47b5b0eb53c389a4a295de4810d6
Aliaksandr Yanushkevich <aliaksandr.yanushkevich@gmail.com>
2019-11-28 00:37:46 +0300

    feat: style control block

--- fancy-weather/index.js
+++ fancy-weather/index.js
@@ -1,49 +1,69 @@
-let control = document.createElement('section'); // create header
-control.className = 'control';
+function init() {
+    let control = document.createElement('section'); // create header
+    control.classList.add('control');
+
+    let buttBlock = document.createElement('div');
+    buttBlock.className = 'buttBlock';
+    control.appendChild(buttBlock);
+
+    const createIconHTML = function (icon_name) { // create icon for button
+        return `<i class="material-icons">${icon_name}</i>`;
+    };
+
+    const changeBackground = document.createElement('button'); // create button for random bg
+    changeBackground.classList.add("button");
+    changeBackground.classList.add("button-changeBackground");
+    changeBackground.innerHTML = createIconHTML('autorenew');
+    
+    buttBlock.appendChild(changeBackground);
+
+    const lang = document.createElement('select'); // create lang list
+    const langArr = ['EN', 'BE', 'RUS'];
+    lang.classList.add("language");
+
+    for (let i = 0; i < langArr.length; i++) {
+        let option = document.createElement('option');
+        option.value = langArr[i];
+        option.text = langArr[i];
+        lang.appendChild(option);
+    }
+    buttBlock.appendChild(lang);
+
+    const tempVal = ['C', 'F']; // create temperature buttons
+
+    const tempCels = document.createElement('button');
+    tempCels.innerText = '℃';
+    tempCels.classList.add('temp');
+    buttBlock.appendChild(tempCels);
+
+    const tempFar = document.createElement('button');
+    tempFar.innerText = '℉';
+    tempFar.classList.add('temp');
+    buttBlock.appendChild(tempFar);
 
 
-const changeBackground = document.createElement('button'); // create button for random bg
-changeBackground.classList.add("button");
-control.appendChild(changeBackground);
+    const searchForm = document.createElement('form'); // create search form
+    searchForm.classList.add('searchForm');
+    const searchField = document.createElement('input');
+    searchField.setAttribute('type', 'text');
+    searchField.classList.add('searchField');
+    searchField.placeholder = "Search city or ZIP";
+    const searchSubmit = document.createElement('input')
+    searchSubmit.setAttribute('type', 'submit');
+    searchSubmit.classList.add('searchSubmit');
+    searchSubmit.value = "SEARCH";
+    searchForm.appendChild(searchField);
+    searchForm.appendChild(searchSubmit);
+    control.appendChild(searchForm);
 
-const lang = document.createElement('select'); // create lang list
-const langArr = ['EN', 'BE', 'RUS'];
-lang.classList.add("language");
+    document.body.appendChild(control);
 
-for (let i = 0; i < langArr.length; i++) {
-    let option = document.createElement('option');
-    option.value = langArr[i];
-    option.text = langArr[i];
-    lang.appendChild(option);
+    // document.getElementsByClassName('control')[0].style.border = '1px solid #0000FF';
+    // document.getElementsByClassName('control')[0].style.width = '100%';
+    // document.getElementsByClassName('control')[0].style.height = '50px';
 }
-control.appendChild(lang);
 
-const tempVal = ['C', 'F']; // create temperature radio button
-for (let i = 0; i < tempVal.length; i++) {
-    const temp = document.createElement('input');
-    temp.setAttribute('type', 'radio');
-    temp.setAttribute('name', 'temp');
-    temp.value = tempVal[i];
-    temp.text = tempVal[i];
-    control.appendChild(temp);
-}
+init();
 
-const searchForm = document.createElement('form');
-const searchField = document.createElement('input');
-searchField.setAttribute('type', 'text');
-const searchSubmit = document.createElement('input')
-searchSubmit.setAttribute('type', 'submit');
-searchForm.appendChild(searchField);
-searchForm.appendChild(searchSubmit);
-control.appendChild(searchForm);
-
-
-
-
-document.body.appendChild(control); 
-
-document.getElementsByClassName('control')[0].style.border = '1px solid #0000FF';
-document.getElementsByClassName('control')[0].style.width = '100%';
-document.getElementsByClassName('control')[0].style.height = '50px';
 
 
