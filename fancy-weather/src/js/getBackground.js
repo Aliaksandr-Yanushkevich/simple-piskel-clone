371b2e54629d5c33a8f6533f660fb6f902756039
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-12-19 20:00:25 +0300

    fix: correct bug with background

--- fancy-weather/src/js/getBackground.js
+++ fancy-weather/src/js/getBackground.js
@@ -12,8 +12,8 @@ export async function getBackground(season, dayTime, currentlyIcon) {
         backgroundImage.crossOrigin = 'Anonymous';
         backgroundImage.src = data.urls.regular;
         backgroundImage.onload = function () {
-            document.getElementsByClassName('wrapper')[0].style.background = `url(${backgroundImage.src}) no-repeat`;
-            document.getElementsByClassName('wrapper')[0].style.backgroundSize = 'cover';
+            document.body.style.background = `url(${backgroundImage.src}) no-repeat`;
+            document.body.style.backgroundSize = 'cover';
         };
     } catch (e) {
         const season = getSeason();
@@ -36,13 +36,13 @@ export async function getBackground(season, dayTime, currentlyIcon) {
                 document.body.style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2016/02/19/11/49/snow-1209991_960_720.jpg)';
                 break;
             case 'spring':
-                document.getElementsByClassName('wrapper')[0].style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2018/01/06/01/01/oak-3064187_960_720.jpg)';
+                document.body.style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2018/01/06/01/01/oak-3064187_960_720.jpg)';
                 break;
             case 'summer':
-                document.getElementsByClassName('wrapper')[0].style.backgroundImage = 'https://cdn.pixabay.com/photo/2016/09/04/20/09/mountains-1645078_960_720.jpg)';
+                document.body.style.backgroundImage = 'https://cdn.pixabay.com/photo/2016/09/04/20/09/mountains-1645078_960_720.jpg)';
                 break;
             case 'autumn':
-                document.getElementsByClassName('wrapper')[0].style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2013/04/03/12/05/tree-99852_960_720.jpg)';
+                document.body.style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2013/04/03/12/05/tree-99852_960_720.jpg)';
                 break;
             default:
                 break;
