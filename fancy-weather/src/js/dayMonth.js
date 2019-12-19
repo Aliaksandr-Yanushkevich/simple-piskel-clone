9898f1f15d87884344436439d3b68a9cb5c52f8d
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-12-19 03:09:19 +0300

    fix: correct RU date&month display

--- fancy-weather/src/js/dayMonth.js
+++ fancy-weather/src/js/dayMonth.js
@@ -2,9 +2,10 @@ export function dayMonth() {
     const timeZone = sessionStorage.getItem('timeZone');
     const time = new Date();
     const localeStandart = localStorage.getItem('lang');
-    const APIMonth = time.toLocaleString('en', { timeZone, month: 'long' });
-    const APIWeekDay = time.toLocaleString('en', { timeZone, weekday: 'short' });
+
     if (localeStandart === 'be') {
+        const APIMonth = time.toLocaleString('en', { timeZone, month: 'long' });
+        const APIWeekDay = time.toLocaleString('en', { timeZone, weekday: 'short' });
         const byWeekDay = {
             Sun: 'Няд',
             Mon: 'Пнд',
@@ -31,6 +32,8 @@ export function dayMonth() {
         };
         document.getElementsByClassName('dayMonth')[0].innerHTML = `${byWeekDay[APIWeekDay]}, ${byMonth[APIMonth]}`;
     } else {
+        const APIMonth = time.toLocaleString(localeStandart, { timeZone, month: 'long' });
+        const APIWeekDay = time.toLocaleString(localeStandart, { timeZone, weekday: 'short' });
         document.getElementsByClassName('dayMonth')[0].innerHTML = `${APIWeekDay}, ${APIMonth}`;
     }
 }
